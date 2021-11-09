ae0af6919925812fee9547e698da7463f7ac694d
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-11-08 16:07:11 -0800

    Reduced file clutter and added pages/components in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, CssBaseline } from '@mui/material'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { NavBar } from './components/NavBar'
+import { Footer } from './components/NavBar'
+
+import { LandingPage } from './pages/LandingPage'
+import { AboutPage } from './pages/AboutPage'
+
+const App = _ => {
+  return(
+    <ThemeProvider>
+      <CssBaseline />
+      <div>
+        <NavBar />
+        <LandingPage />
+        <AboutPage />
+        <Footer />
+      </div>
+    </ThemeProvider>
+  )
 }
-
-export default App;
