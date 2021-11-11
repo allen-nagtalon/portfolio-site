85dd164f6d3e64f7e4dbde9f8eb21846291f83d3
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-11-11 14:56:34 -0800

    Added content to Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,28 @@
+import React from 'react'
+import { AppBar, Toolbar, Box, Typography } from '@mui/material'
+
 const Footer = _ => {
   return (
-    <div>This is the Footer.</div>
+    <AppBar 
+      position='static'
+      elevation={0}
+      color='transparent'
+      sx={{ 
+        px: 6,
+        py: 6
+      }}
+    >
+      <Toolbar>
+        <Box>
+          <Typography component='h1'>
+            Like what you see? Let's get in contact!
+          </Typography>
+          <Typography component='h3'>
+            Drop me a line at ant.nagtalon22@gmail.com. Alternatively, you can also find me on LinkedIn.
+          </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
