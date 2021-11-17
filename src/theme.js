6d1151ef9b0a7f903971fe89c336f1339692b1bf
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-11-16 22:11:52 -0800

    Created theme

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,51 @@
-import { createTheme } from '@mui/material'
+import { createTheme, responsiveFontSizes } from '@mui/material'
 
 const primary = '#000000'
-const secondary = '#000000'
+const secondary = '#F6A192'
+const white = '#FFFFFF'
 
-const theme = createTheme({
+let theme = createTheme({
   palette: {
     primary: { 
       main: primary 
     },
     secondary: { 
       main: secondary 
+    },
+    white: {
+      main: white
+    }
+  },
+  typography: {
+    fontFamily: [
+      'Playfair Display',
+      'Roboto'
+    ].join(','),
+    h1: {
+      fontFamily: 'Roboto',
+      fontSize: '1.8rem',
+      fontWeight: 'bolder'
+    },
+    h3: {
+      fontFamily: 'Roboto',
+      fontSize: '1.8rem',
+      fontWeight: 'bold',
+    },
+    h5: {
+      fontFamily: 'Roboto',
+      fontSize: '1.2rem'
+    },
+    mainlogo: {
+      fontFamily: 'Playfair Display',
+      fontSize: '1.8rem',
+    },
+    navlink: {
+      fontFamily: 'Roboto',
+      fontSize: '1.2rem',
     }
   }
 })
+
+theme = responsiveFontSizes(theme)
 
 export default theme
