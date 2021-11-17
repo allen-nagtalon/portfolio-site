10dc5c42a3fa93f11d1227d0a1f2e21e01767f24
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-11-16 22:12:12 -0800

    Created basic LandingPage

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,15 +1,30 @@
-import { Container } from '@mui/material'
+import { Box, Container, Typography } from '@mui/material'
 
 const LandingPage = _ => {
   return (
-    <Container
+    <Box
+      component='div'
       sx={{
+        display: 'flex',
         width: '100vw',
-        height: '100vh'
+        height: '100vh',
+        alignItems: 'center',
+        bgcolor: 'secondary.main'
       }}
     >
-      This is the Landing page.
-    </Container>
+      <Container 
+        maxWidth='md'
+      >
+        <Typography 
+          variant='h3'
+          sx={{ 
+            pr: 42
+          }}
+        >
+          Hi, I'm Allen — a junior software engineer with a great passion for designing products that make things simple & obvious.
+        </Typography>
+      </Container>
+    </Box>
   )
 }
 
