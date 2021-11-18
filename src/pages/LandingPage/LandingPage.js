6c90893aadc6b7b7e3759add60f53156f42ff81c
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-11-18 13:47:02 -0800

    Adjusted LandingPage spacing

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -9,17 +9,16 @@ const LandingPage = _ => {
         width: '100vw',
         height: '100vh',
         alignItems: 'center',
+        justifyContent: 'center',
         bgcolor: 'secondary.main'
       }}
     >
       <Container 
         maxWidth='md'
       >
-        <Typography 
-          variant='h3'
-          sx={{ 
-            pr: 42
-          }}
+        <Typography
+          component='div'
+          variant='landing'
         >
           Hi, I'm Allen — a junior software engineer with a great passion for designing products that make things simple & obvious.
         </Typography>
