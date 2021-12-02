8df6d26dcb0606d049261a5e1bfecebfde550f61
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-12-02 13:50:31 -0800

    Adjusted footer spacing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { AppBar, Toolbar, Container, Grid, Typography } from '@mui/material'
+import { Box } from '@mui/system'
 
 const Footer = _ => {
   return (
@@ -13,16 +14,13 @@ const Footer = _ => {
       <Toolbar>
         <Container maxWidth='md'>
           <Grid container>
-            <Grid item xs={7}>
-              <Typography variant='h1' mb={2}>
+            <Grid item>
+              <Typography variant='h1' mb={4}>
                 Let's get in contact!
               </Typography>
               <Typography variant='h5'>
                 Drop me a line at ant.nagtalon22@gmail.com. Alternatively, you can also find me on LinkedIn.
               </Typography>
-            </Grid>
-            <Grid item xs={5}>
-              
             </Grid>
           </Grid>
         </Container>
